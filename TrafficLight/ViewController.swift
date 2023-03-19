54ebb3f40e3e3a9b8b5d359f20d50e66ec22d315
Nadya <hoppe.nadya@gmail.com>
2023-03-19 11:00:03 -0700

    Setup traffic light color changing

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,17 +15,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         trafficLightRed.layer.cornerRadius = 50
         trafficLightYellow.layer.cornerRadius = 50
         trafficLightGreen.layer.cornerRadius = 50
         startButton.layer.cornerRadius = 10
-    
     }
-
+    
     @IBAction func startButtonDidTapped(_ sender: Any) {
-        startButton.setTitle("NEXT", for: .normal)
+        if trafficLightRed.layer.opacity == 0.3
+            && trafficLightYellow.layer.opacity == 0.3
+            && trafficLightGreen.layer.opacity == 0.3 {
+            startButton.setTitle("NEXT", for: .normal)
+            trafficLightRed.layer.opacity = 1.0
+        } else if trafficLightRed.layer.opacity == 1.0 {
+            trafficLightRed.layer.opacity = 0.3
+            trafficLightYellow.layer.opacity = 1.0
+        } else if trafficLightYellow.layer.opacity == 1.0 {
+            trafficLightYellow.layer.opacity = 0.3
+            trafficLightGreen.layer.opacity = 1.0
+        } else {
+            trafficLightGreen.layer.opacity = 0.3
+            trafficLightRed.layer.opacity = 1.0
+        }
     }
-    
 }
-
