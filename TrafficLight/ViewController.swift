89b2ee078d79abcde62d6670bca1900f47f207f4
Nadya <hoppe.nadya@gmail.com>
2023-03-18 21:59:59 -0700

    Setup color view objects

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var trafficLightRed: UIView!
+    @IBOutlet var trafficLightYellow: UIView!
+    @IBOutlet var trafficLightGreen: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        trafficLightRed.layer.cornerRadius = 50
+        trafficLightYellow.layer.cornerRadius = 50
+        trafficLightGreen.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
+    
     }
 
-
+    @IBAction func startButtonDidTapped(_ sender: Any) {
+        startButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
