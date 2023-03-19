18745bfcbc33faf0a5942da739365c5f16085383
Nadya <hoppe.nadya@gmail.com>
2023-03-19 14:19:47 -0700

    Setup corner radius for view shapes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        trafficLightRed.layer.cornerRadius = 50
-        trafficLightYellow.layer.cornerRadius = 50
-        trafficLightGreen.layer.cornerRadius = 50
+        trafficLightRed.layer.cornerRadius = trafficLightRed.frame.width / 2
+        trafficLightYellow.layer.cornerRadius = trafficLightYellow.frame.width / 2
+        trafficLightGreen.layer.cornerRadius = trafficLightGreen.frame.width / 2
         startButton.layer.cornerRadius = 10
     }
     
@@ -27,12 +27,15 @@ class ViewController: UIViewController {
             && trafficLightGreen.layer.opacity == 0.3 {
             startButton.setTitle("NEXT", for: .normal)
             trafficLightRed.layer.opacity = 1.0
+            
         } else if trafficLightRed.layer.opacity == 1.0 {
             trafficLightRed.layer.opacity = 0.3
             trafficLightYellow.layer.opacity = 1.0
+            
         } else if trafficLightYellow.layer.opacity == 1.0 {
             trafficLightYellow.layer.opacity = 0.3
             trafficLightGreen.layer.opacity = 1.0
+            
         } else {
             trafficLightGreen.layer.opacity = 0.3
             trafficLightRed.layer.opacity = 1.0
